4e92c191a00d50150bbcf828c3388762d2b5d802
李健 <lijian@lijiandeMacBook-Pro.local>
2021-09-11 11:46:21 +0800

    学习list渲染

---
+++ lib/model/post.dart
@@ -0,0 +1,100 @@
+class Post {
+  const Post({this.title = '', this.author = '', this.imageUrl: ''});
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'Candy Shop',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Childhood in a picture',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Contained',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Free Hugs',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Gravity Falls',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Icecream Truck',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'keyclack',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Overkill',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Say Hello to Barry',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Space Skull',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'The Old Fashioned',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Tornado',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'Undo',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  ),
+  Post(
+    title: 'White Dragon',
+    author: 'Mohamed Chahin',
+    imageUrl:
+        'https://ns-strategy.cdn.bcebos.com/ns-strategy/upload/fc_big_pic/part-00311-4070.jpg',
+  )
+];
